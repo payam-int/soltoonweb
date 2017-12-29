fb792af2be9268d1e473d5793f7c23d9e7d80947
Payam Mohammadi <payam.int@gmail.com>
2017-12-29 19:19:04 +0330

    SNAPSHOT

--- soltoonweb/soltoonweb/settings/production.py
+++ soltoonweb/soltoonweb/settings/production.py
@@ -11,3 +11,19 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 25
 DEFAULT_FROM_EMAIL = 'Soltoon <net@soltoon.net>'
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'soltoon',
+        'USER': os.environ['SOLTOON_DB_USERNAME'],
+        'PASSWORD': os.environ['SOLTOON_DB_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+SECURE_SSL_REDIRECT = True
