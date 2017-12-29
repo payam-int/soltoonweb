06a0ef3a2a9137854ff48c16f2a417d762ca083b
Payam Mohammadi <payam.int@gmail.com>
2017-12-29 18:53:36 +0330

    SNAPSHOT

--- soltoonweb/soltoonweb/settings/production.py
+++ soltoonweb/soltoonweb/settings/production.py
@@ -3,8 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*.soltoon.net']
-
+ALLOWED_HOSTS = ['*.soltoon.net', 'soltoon.net']
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = '127.0.0.1'
