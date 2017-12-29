e5760d9894a1076d6ec2b31c76c68a0375f6a8f0
Payam Mohammadi <payam.int@gmail.com>
2017-12-29 19:55:54 +0330

    SNAPSHOT

--- soltoonweb/sandbox/admin.py
+++ soltoonweb/sandbox/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib.admin import ModelAdmin
 
-from sandbox.models import TrainingScenario
-
-
+from sandbox.models import TrainingScenario, TrainingScenarioCode
 
 admin.site.register(TrainingScenario)
+admin.site.register(TrainingScenarioCode)
