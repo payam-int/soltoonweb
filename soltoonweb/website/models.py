ab98ca6d9722369b64d9693635abb579d9df9d44
Payam Mohammadi <payam.int@gmail.com>
2017-12-30 22:17:36 +0330

    SNAPSHOT

--- soltoonweb/website/models.py
+++ soltoonweb/website/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, editable=False)
+    status = models.BooleanField(verbose_name=_("published"))
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
